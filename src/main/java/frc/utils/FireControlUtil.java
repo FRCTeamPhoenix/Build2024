66d24984a375a6e0cc1f73b31a5a993eaee89c44
Software2342 <159857809+Software2342@users.noreply.github.com>
2024-03-02 10:50:49 -0500

    .

--- src/main/java/frc/utils/FireControlUtil.java
+++ src/main/java/frc/utils/FireControlUtil.java
@@ -15,18 +15,17 @@ public class FireControlUtil {
 
     public FireControlUtil(boolean allianceIsRed){
         if (allianceIsRed) speakerPose = Constants.VisionConstants.kTagLayout.getTagPose(4).get().toPose2d();
-        else speakerPose = Constants.VisionConstants.kTagLayout.getTagPose(7).get().toPose2d();
+        else speakerPose = Constants.VisionConstants.kTagLayout.getTagPose(7).get().toPose2d(); // Default to blue alliance
 
         //TODO: Put the new values from the arm here
         interpolator.put(0.0, 0.0);
 
         //TODO: Get actual values for the PID controller
-        pidController = new PIDController(0.0, 0.0, 0.0);
+        pidController = new PIDController(0.1, 0.0, 0.0);
     }
-    public double getShooterAngle(Pose2d robotPose){
+    public double getShooterAngle(Pose2d robotPose) {
         Transform2d transformToSpeaker = robotPose.minus(speakerPose);
-        double distance = Math.sqrt((transformToSpeaker.getX() * transformToSpeaker.getX())
-                + transformToSpeaker.getY() * transformToSpeaker.getY());
+        double distance = Math.sqrt(Math.pow(transformToSpeaker.getX(), 2) + Math.pow(transformToSpeaker.getY(), 2));
         return interpolator.get(distance);
     }
 
