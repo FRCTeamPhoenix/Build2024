b2ba7a796a79741a1b174a1df9e8a88991e17fd8
Software2342 <159857809+Software2342@users.noreply.github.com>
2024-03-03 10:07:13 -0500

    Note pose initial work
    03/02/2024

--- src/main/java/frc/utils/NotePoseGenerator.java
+++ src/main/java/frc/utils/NotePoseGenerator.java
@@ -5,10 +5,12 @@ import edu.wpi.first.math.geometry.Rotation2d;
 
 public class NotePoseGenerator {
 
-    public static Pose2d generateNotePose(OakCameraObject trackedNote){
+    public static Pose2d generateNotePose(OakCameraObject trackedNote, Pose2d currentPose){
         double x = trackedNote.getHorizontalDistance() * Math.cos(Math.toRadians(trackedNote.getXAngle()));
+        x = (x/1000) + currentPose.getX();
         double y = trackedNote.getHorizontalDistance() * Math.sin(Math.toRadians(trackedNote.getXAngle()));
-        Pose2d notePose = new Pose2d(x, y, new Rotation2d(Math.toRadians(trackedNote.getXAngle())));
+        y = (y/1000) + currentPose.getY();
+        Pose2d notePose = new Pose2d(x, y, new Rotation2d(Math.toRadians(trackedNote.getXAngle())).plus(currentPose.getRotation()));
         return notePose;
     }
 }
